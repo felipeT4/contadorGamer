82d3875c6f9c0d633507128c7e6ba2f82cbf7055
felipe_T4 <felipetavares2503@gmail.com>
2026-05-21 10:06:02 -0300

    atividade 21/05

--- App.js
+++ App.js
@@ -1,166 +1,40 @@
-//Importa o React e o useState
-//useState serve para criar valores que podem mudar a tela
-import React, {useState} from "react";
-//importa componentes nativos do react native
-import {
-  View,                         // Caixa / Área da tela
-  Text,                        // Textos
-  Button,                     // Botão
-  StyleSheet,                // Estilos
-  TouchableOpacity          // Botão customizável
-} from 'react-native';
+//Importação para o menu lateral funcionar
+import 'react-native-gesture-handler';
 
-// Componente principal APP
+//importar o container da navegação 
+// ele controla todas as telas do app 
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App(){
-  // Cria um estado (variável PHP) chamado "pontos"
-  // pontos = valor atual 
-  // setPontos = função para alterar o valor 
-  // 0 = valor inicial
-  const [pontos,setPontos] = useState(0);
+// importar o menu lateral (DRAWER)
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
+//importar as telas
+import Jogador1 from './components/Jogador1';
 
- // Função para aumentar 1 ponto
-  function aumentar(){
-  // Pega o valor auto e soma +1
-    setPontos(pontos+1);
-  }
+// Cria o DRAWER 
+const Drawer = createDrawerNavigator();
 
+//componente principal do APP
+export default function Jogador1(){
+  /// o que está dentro do return aparece na tela 
+  return (
+   // container Principal da navegação 
+   <NavigationContainer>
+    {/* menu lateral*/}
+    <Drawer.Navigator>
+      {/*tela do jogador1*/}
+      <Drawer.Screen 
+      // nome que aparece no menu
+      name="1 Jogador"
+      // componente que será aberto
+      component={Jogador1}
+      />
+    
 
-  // Função para diminuir 1 ponto
-  function diminuir(){
-  // Pega o valor auto e subtrai -1
-    setPontos(pontos-1);
-  }
+    </Drawer.Navigator>
 
-
- // Função para resetar o contador
-  function resetar(){
-    // volta o valor para 0
-    setPontos(0);
-  }
-
-
-
-   // tudo que está no RETURN aparece na tela
-  return(
-    // View proncipal do APP
-    <View style={styles.container}>
-      {/* Título do APP */}
-      <Text style={styles.titulo}>
-        🎮 Contador Gamer
-      </Text>
-      {/* Mostra o valor dos pontos */}
-      <Text style={styles.pontos}>{pontos}</Text>
-      {/* área dos botões +1 e -1 */}
-      <View style={styles.areaBotoes}>
-       <TouchableOpacity 
-       onPress={aumentar}
-       style={styles.botao}>
-        <Text style={styles.textoBotao}>+1</Text>
-       </TouchableOpacity>
-
-       <TouchableOpacity 
-       onPress={diminuir}
-       style={styles.botao}>
-        <Text style={styles.textoBotao}>-1</Text>
-       </TouchableOpacity>
-
-      
-      </View>
-
-      <TouchableOpacity 
-       onPress={resetar}
-       style={styles.botaoReset}>
-        <Text style={styles.textoBotao}>Reset</Text>
-       </TouchableOpacity>
-
-    </View>
+   </NavigationContainer> 
   );
- }
-
- // ÁREA DE ESTILOS DO APP 
- const styles = StyleSheet.create({
-  // ESTILO DA TELA PRINCIPAL
-  container:{
-    // OCUPA A TELA TODA 
-    flex:1,
-    // COR DE FUNDO
-    backgroundColor:'#121212',
-    // CENTRALIZA HORIZONTALMENTE
-    alignItems:'center',
-    // CENTRALIZA VERTICALMENTE
-    justifyContent:'center',
-    // ESPAÇAMENTO INTERNO
-    padding:20,
-  },
-
-   // ESTILO DO TÍTULO
-  titulo:{
-    // TAMANHO DA FONTE
-    fontSize:32,
-    // COR DO TEXTO
-    color:'#00ff88',
-    // TEXTO EM NEGRITO
-    fontWeight:'bold',
-    // ESPAÇO ABAIXO
-    marginBottom:30,
-  },
-
-  // ESTILO DOS PONTOS
-  pontos:{
-    // TAMANHO GRANDE (¬‿¬)
-    fontSize:80,
-    // COR BRANCA
-    color:'#ffffffff',
-    // ESPAÇO ABAIXO
-    marginBottom:40,
-    // NEGRITO
-    fontWeight:'bold',
-  },
-
- // ÁREA DOS BOTÕES 
- areaBotoes:{
-    // DEIXA UM BOTÃO AO LADO DO OUTRO
-    flexDirection: 'row',
-    // ESPAÇO ABAIXO
-    marginBottom:20,
- },
-   // ESTILOS DOS BOTÕES +1 E -1
- botao:{
-   // COR DO BOTÃO 
-  backgroundColor:'#00ff88',
-   // ESPAÇAMENTO VERTICAL
-  paddingVertical:15,
-   // ESPAÇAMENTO HORIZONTAL
- paddingHorizontal:30,
-  // ARREDONDA AS BORDAS
- borderRadius:12,
-  // ESPAÇO ENTRE OS BOTÕES
- marginHorizontal:10,
- },
-
- //ESTILO DO BOTÃO RESETAR
- botaoReset:{
-  // COR VERMELHA
-  backgroundColor:'#ff3b30',
-  // ESPAÇAMENTOS
-  paddingVertical:15,
-  paddingHorizontal:40,
-  // ARREDONDA AS BORDAS
-  borderRadius:12,
- },
-
- // ESTILO DOS TEXTOS DOS BOTÕES 
- textoBotao:{
-  // TAMANHO DA LETRA
-  fontSize:22,
-  // COR DO TEXTO
-  color:'#000',
-  // NEGRITO
-  fontWeight:'bold',
-
- },
-
-
- });
+  
+  
+}
