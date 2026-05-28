37ef2131862d5db5391062976352a248fe908f22
felipe_T4 <felipetavares2503@gmail.com>
2026-05-28 10:09:18 -0300

    Correção ContadorGamer

---
+++ components/Jogador2-atividade.js
@@ -0,0 +1,271 @@
+//Importa o React e o useState
+//useState serve para criar valores que podem mudar a tela
+import React, {useState} from "react";
+//importa componentes nativos do react native
+import {
+  View,                         // Caixa / Área da tela
+  Text,                        // Textos
+  Button,                     // Botão
+  StyleSheet,                // Estilos
+  TouchableOpacity          // Botão customizável
+} from 'react-native';
+import Jogador1 from "../App";
+
+// Componente principal APP
+
+export default function App(){
+  // Cria um estado (variável PHP) chamado "pontos"
+  // pontos = valor atual 
+  // setPontos = função para alterar o valor 
+  // 0 = valor inicial
+  const [pontos,setPontos] = useState(0);
+  const [pontos2,setPontos2] = useState(0);
+
+
+
+ // Função para aumentar 1 ponto
+  function aumentar(){
+  // Pega o valor auto e soma +1
+    setPontos(pontos+1);
+  }
+
+
+  // Função para diminuir 1 ponto
+  function diminuir(){
+  // Pega o valor auto e subtrai -1
+    setPontos(pontos-1);
+  }
+
+
+ // Função para resetar o contador
+  function resetar(){
+    // volta o valor para 0
+    setPontos(0);
+  }
+
+  if 
+    (pontos < 0){
+      setPontos(0)
+    }
+
+/*-------------------------------------------------*/
+  function aumentar2(){
+  // Pega o valor auto e soma +1
+    setPontos2(pontos2+1);
+  }
+
+
+  // Função para diminuir 1 ponto
+  function diminuir2(){
+  // Pega o valor auto e subtrai -1
+    setPontos2(pontos2-1);
+  }
+
+
+ // Função para resetar o contador
+  function resetar2(){
+    // volta o valor para 0
+    setPontos2(0);
+  }
+
+  if 
+    (pontos2 < 0){
+      setPontos2(0)
+    }
+  
+/*-----------------------------------------------*/
+
+
+
+
+   // tudo que está no RETURN aparece na tela
+  return(
+    // View proncipal do APP
+    <View style={styles.container}>
+      {/* Título do APP */}
+      <Text style={styles.titulo}>
+        👾 Contador Gamer
+      </Text>
+     
+     <View style={styles.t}>
+        </View>
+
+        
+      <Text style={styles.titulo}>
+        Jogador1
+      </Text>
+
+      
+     
+      {/* Mostra o valor dos pontos */}
+      <Text style={styles.pontos}>{pontos}</Text> 
+       <View style={styles.Jogador1}>
+      
+      {/* área dos botões +1 e -1 */}
+      <View style={styles.areaBotoes}>
+       <TouchableOpacity 
+       onPress={aumentar}
+       style={styles.botao}>
+        <Text style={styles.textoBotao}>+1</Text>
+       </TouchableOpacity>
+
+       <TouchableOpacity 
+       onPress={diminuir}
+       style={styles.botao}>
+        <Text style={styles.textoBotao}>-1</Text>
+       </TouchableOpacity>
+      </View>
+      </View>
+
+     
+
+      <TouchableOpacity 
+       onPress={resetar}
+       style={styles.botaoReset}>
+        <Text style={styles.textoBotao}>Reset</Text>
+       </TouchableOpacity>
+       
+
+{/*-------------------------------------------------------------------------------------------*/}
+    
+      <Text style={styles.pontos}>{pontos2}</Text> 
+      
+      
+     <View style={styles.Jogador2}>
+      {/* área dos botões +1 e -1 */}
+      <View style={styles.areaBotoes}>
+       <TouchableOpacity 
+       onPress={aumentar2}
+       style={styles.botao}>
+        <Text style={styles.textoBotao}>+1</Text>
+       </TouchableOpacity>
+
+       <TouchableOpacity 
+       onPress={diminuir2}
+       style={styles.botao}>
+        <Text style={styles.textoBotao}>-1</Text>
+       </TouchableOpacity>
+        </View>
+        </View>
+    
+     
+
+      <TouchableOpacity 
+       onPress={resetar2}
+       style={styles.botaoReset}>
+        <Text style={styles.textoBotao}>Reset</Text>
+       </TouchableOpacity>
+       
+
+{/*----------------------------------------------------------------------------------------*/}
+
+
+
+
+      
+
+    </View>
+  );
+
+ }
+
+
+ // ÁREA DE ESTILOS DO APP 
+ const styles = StyleSheet.create({
+  // ESTILO DA TELA PRINCIPAL
+  container:{
+    // OCUPA A TELA TODA
+     
+    flex:1,
+    // COR DE FUNDO
+    backgroundColor:'#002d4fff',
+    // CENTRALIZA HORIZONTALMENTE
+    alignItems:'center',
+    // CENTRALIZA VERTICALMENTE
+    justifyContent:'center',
+    // ESPAÇAMENTO INTERNO
+    padding:20,
+  },
+  t:{
+    flexDirection: 'row'
+  },
+  Jogador1:{
+    flexDirection: 'column',
+  },
+    Jogador2:{
+    flexDirection: 'column',
+  },
+
+
+   // ESTILO DO TÍTULO
+  titulo:{
+    // TAMANHO DA FONTE
+    fontSize:32,
+    // COR DO TEXTO
+    color:'#00a6ffff',
+    // TEXTO EM NEGRITO
+    fontWeight:'bold',
+    // ESPAÇO ABAIXO
+    marginBottom:30,
+  },
+
+  // ESTILO DOS PONTOS
+  pontos:{
+
+    // TAMANHO GRANDE (¬‿¬)
+    fontSize:80,
+    // COR BRANCA
+    color:'#ffffffff',
+    // ESPAÇO ABAIXO
+    marginBottom:40,
+    // NEGRITO
+    fontWeight:'bold',
+  },
+
+ // ÁREA DOS BOTÕES 
+ areaBotoes:{
+
+    // DEIXA UM BOTÃO AO LADO DO OUTRO
+    flexDirection: 'row',
+    // ESPAÇO ABAIXO
+    marginBottom:20,
+ },
+   // ESTILOS DOS BOTÕES +1 E -1
+ botao:{
+
+   // COR DO BOTÃO 
+  backgroundColor:'#41de4cff',
+   // ESPAÇAMENTO VERTICAL
+  paddingVertical:15,
+   // ESPAÇAMENTO HORIZONTAL
+ paddingHorizontal:30,
+  // ARREDONDA AS BORDAS
+ borderRadius:12,
+  // ESPAÇO ENTRE OS BOTÕES
+ marginHorizontal:10,
+ },
+
+ //ESTILO DO BOTÃO RESETAR
+ botaoReset:{
+  // COR VERMELHA
+  backgroundColor:'#d40b00ff',
+  // ESPAÇAMENTOS
+  paddingVertical:15,
+  paddingHorizontal:40,
+  // ARREDONDA AS BORDAS
+  borderRadius:12,
+ },
+
+ // ESTILO DOS TEXTOS DOS BOTÕES 
+ textoBotao:{
+  // TAMANHO DA LETRA
+  fontSize:22,
+  // COR DO TEXTO
+  color:'#181818ff',
+  // NEGRITO
+  fontWeight:'bold',
+
+ },
+
+
+ });
--- components/Jogador2.js
+++ components/Jogador2.js
@@ -1,267 +1,304 @@
 //Importa o React e o useState
 //useState serve para criar valores que podem mudar a tela
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 //importa componentes nativos do react native
 import {
   View,                         // Caixa / Área da tela
   Text,                        // Textos
   Button,                     // Botão
   StyleSheet,                // Estilos
-  TouchableOpacity          // Botão customizável
+  TouchableOpacity,         // Botão customizável
+  TextInput         
 } from 'react-native';
-import Jogador1 from "../App";
 
 // Componente principal APP
 
-export default function App(){
+export default function Jogador2() {
   // Cria um estado (variável PHP) chamado "pontos"
   // pontos = valor atual 
   // setPontos = função para alterar o valor 
   // 0 = valor inicial
-  const [pontos,setPontos] = useState(0);
-  const [pontos2,setPontos2] = useState(0);
+  const [pontos1, setPontos1] = useState(0);
+  const [pontos2, setPontos2] = useState(0);
+  const [nome1, setNome1] = useState('Jogador 1');
+  const [nome2, setNome2] = useState('Jogador 2');
+  const [lider, setLider] = useState('Empate');
 
-
-
- // Função para aumentar 1 ponto
-  function aumentar(){
-  // Pega o valor auto e soma +1
-    setPontos(pontos+1);
-  }
-
-
-  // Função para diminuir 1 ponto
-  function diminuir(){
-  // Pega o valor auto e subtrai -1
-    setPontos(pontos-1);
-  }
-
-
- // Função para resetar o contador
-  function resetar(){
-    // volta o valor para 0
-    setPontos(0);
-  }
-
-  if 
-    (pontos < 0){
-      setPontos(0)
+  // Executar alguma ação automaticamente
+ // quando algo mudar
+// nesse caso
+    // - toda vez que o os pontos / nome mudarem
+   // - o código dentro do useEffect será executado 
+  // 
+  useEffect(() => {
+    if(pontos1 > pontos2){
+        setLider(nome1);
     }
+    else if (pontos2 > pontos1){
+        setLider(nome2);
+    }
+    else {
+        setLider('Empate');
+    }
+  }, [pontos1, pontos2, nome1, nome2]);
+  //dependencia que disparam o useEffect quando mudam
 
-/*-------------------------------------------------*/
-  function aumentar2(){
-  // Pega o valor auto e soma +1
-    setPontos2(pontos2+1);
+
+  // Função para aumentar 1 ponto
+  function aumentar1() {
+    // Pega o valor auto e soma +1
+    setPontos1(pontos1 + 100000);
   }
+
+  // Função para aumentar 1 ponto
+  function aumentar2() {
+    // Pega o valor auto e soma +1
+    setPontos2(pontos2 + 1);
+  }
+
 
 
   // Função para diminuir 1 ponto
-  function diminuir2(){
-  // Pega o valor auto e subtrai -1
-    setPontos2(pontos2-1);
+  function diminuir1() {
+    // Pega o valor auto e subtrai -1
+    if (pontos1 > 0) {
+    setPontos1(pontos1 - 1);
+    }
+  }
+
+  // Função para diminuir 1 ponto
+  function diminuir2() {
+    // Pega o valor auto e subtrai -1
+        if (pontos2 > 0) {
+    setPontos2(pontos2 - 1);
+    }
   }
 
 
- // Função para resetar o contador
-  function resetar2(){
+
+  // Função para resetar o contador
+  function resetar() {
     // volta o valor para 0
+    setPontos1(0);
     setPontos2(0);
+    setLider('Empate');
+
   }
-
-  if 
-    (pontos2 < 0){
-      setPontos2(0)
-    }
-  
-/*-----------------------------------------------*/
+ 
 
 
 
 
-   // tudo que está no RETURN aparece na tela
-  return(
+
+
+  // tudo que está no RETURN aparece na tela
+  return (
     // View proncipal do APP
     <View style={styles.container}>
       {/* Título do APP */}
       <Text style={styles.titulo}>
-        👾 Contador Gamer
+        👾 Contador Gamer - Modo 2 Jogadores
       </Text>
-     
-     <View style={styles.t}>
-        </View>
-
-        
-      <Text style={styles.titulo}>
-        Jogador1
+      <Text style={styles.lider}>
+        🏆 Líder: {lider}
       </Text>
+      {/* area dos jogadores */}
+      <View style={styles.areaJogadores}>
 
-      
-     
-      {/* Mostra o valor dos pontos */}
-      <Text style={styles.pontos}>{pontos}</Text> 
-       <View style={styles.Jogador1}>
-      
-      {/* área dos botões +1 e -1 */}
-      <View style={styles.areaBotoes}>
-       <TouchableOpacity 
-       onPress={aumentar}
-       style={styles.botao}>
-        <Text style={styles.textoBotao}>+1</Text>
-       </TouchableOpacity>
+        {/* Jogador 1 */}
+        <View style={styles.areaJogador}>
+        <TextInput 
+            style={styles.input}
+            placeholder="Nome do jogador 1"
+            placeholderTextColor="#999"
+            value={nome1}
+            onChangeText={setNome1}
+        />
+        <Text style={styles.nome}> {nome1}</Text>
 
-       <TouchableOpacity 
-       onPress={diminuir}
-       style={styles.botao}>
-        <Text style={styles.textoBotao}>-1</Text>
-       </TouchableOpacity>
-      </View>
-      </View>
+          <Text style={styles.pontos}>{pontos1}</Text>
+          {/* área dos botões +1 e -1 */}
+          <View style={styles.areaBotoes}>
+            <TouchableOpacity
+              onPress={aumentar1}
+              style={styles.botao}>
+              <Text style={styles.textoBotao}>+1</Text>
+            </TouchableOpacity>
 
-     
+            <TouchableOpacity
+              onPress={diminuir1}
+              style={styles.botao}>
+              <Text style={styles.textoBotao}>-1</Text>
+            </TouchableOpacity>
 
-      <TouchableOpacity 
-       onPress={resetar}
-       style={styles.botaoReset}>
-        <Text style={styles.textoBotao}>Reset</Text>
-       </TouchableOpacity>
-       
 
-{/*-------------------------------------------------------------------------------------------*/}
-   
-      <Text style={styles.pontos}>{pontos2}</Text> 
-      
-     <View style={styles.Jogador2}>
-      {/* área dos botões +1 e -1 */}
-      <View style={styles.areaBotoes}>
-       <TouchableOpacity 
-       onPress={aumentar2}
-       style={styles.botao}>
-        <Text style={styles.textoBotao}>+1</Text>
-       </TouchableOpacity>
+          </View>
 
-       <TouchableOpacity 
-       onPress={diminuir2}
-       style={styles.botao}>
-        <Text style={styles.textoBotao}>-1</Text>
-       </TouchableOpacity>
         </View>
+        {/* Jogador2 */}
+        <View style={styles.areaJogador}>
+            <TextInput 
+            style={styles.input}
+            placeholder="Nome do jogador 2"
+            placeholderTextColor="#999"
+            value={nome2}
+            onChangeText={setNome2}
+        />
+        <Text style={styles.nome}> {nome2}</Text>
+          <Text style={styles.pontos}>{pontos2}</Text>
+          {/* área dos botões +1 e -1 */}
+          <View style={styles.areaBotoes}>
+            <TouchableOpacity
+              onPress={aumentar2}
+              style={styles.botao}>
+              <Text style={styles.textoBotao}>+1</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={diminuir2}
+              style={styles.botao}>
+              <Text style={styles.textoBotao}>-1</Text>
+            </TouchableOpacity>
+
+
+          </View>
+
         </View>
-    
-     
+      </View>
 
-      <TouchableOpacity 
-       onPress={resetar2}
-       style={styles.botaoReset}>
+
+      <TouchableOpacity
+        onPress={resetar}
+        style={styles.botaoReset}>
         <Text style={styles.textoBotao}>Reset</Text>
-       </TouchableOpacity>
-       
-
-{/*----------------------------------------------------------------------------------------*/}
-
-
-
-
-      
+      </TouchableOpacity>
 
     </View>
   );
+}
 
- }
 
-
- // ÁREA DE ESTILOS DO APP 
- const styles = StyleSheet.create({
+// ÁREA DE ESTILOS DO APP 
+const styles = StyleSheet.create({
   // ESTILO DA TELA PRINCIPAL
-  container:{
-    // OCUPA A TELA TODA
-     
-    flex:1,
+  container: {
+    // OCUPA A TELA TODA 
+    flex: 1,
     // COR DE FUNDO
-    backgroundColor:'#002d4fff',
+    backgroundColor: '#113856ff',
     // CENTRALIZA HORIZONTALMENTE
-    alignItems:'center',
+    alignItems: 'center',
     // CENTRALIZA VERTICALMENTE
-    justifyContent:'center',
+    justifyContent: 'center',
     // ESPAÇAMENTO INTERNO
-    padding:20,
-  },
-  t:{
-    flexDirection: 'row'
-  },
-  Jogador1:{
-    flexDirection: 'column',
-  },
-    Jogador2:{
-    flexDirection: 'column',
+    padding: 20,
   },
 
-
-   // ESTILO DO TÍTULO
-  titulo:{
+  // ESTILO DO TÍTULO
+  titulo: {
     // TAMANHO DA FONTE
-    fontSize:32,
+    fontSize: 32,
     // COR DO TEXTO
-    color:'#00a6ffff',
+    color: '#00a6ffff',
     // TEXTO EM NEGRITO
-    fontWeight:'bold',
+    fontWeight: 'bold',
     // ESPAÇO ABAIXO
-    marginBottom:30,
+    marginBottom: 30,
   },
 
   // ESTILO DOS PONTOS
-  pontos:{
+  pontos: {
     // TAMANHO GRANDE (¬‿¬)
-    fontSize:80,
+    fontSize: 80,
     // COR BRANCA
-    color:'#ffffffff',
+    color: '#ffffffff',
     // ESPAÇO ABAIXO
-    marginBottom:40,
+    marginBottom: 40,
     // NEGRITO
-    fontWeight:'bold',
+    fontWeight: 'bold',
   },
 
- // ÁREA DOS BOTÕES 
- areaBotoes:{
+  // ÁREA DOS BOTÕES 
+  areaBotoes: {
     // DEIXA UM BOTÃO AO LADO DO OUTRO
     flexDirection: 'row',
     // ESPAÇO ABAIXO
-    marginBottom:20,
- },
-   // ESTILOS DOS BOTÕES +1 E -1
- botao:{
-   // COR DO BOTÃO 
-  backgroundColor:'#41de4cff',
-   // ESPAÇAMENTO VERTICAL
-  paddingVertical:15,
-   // ESPAÇAMENTO HORIZONTAL
- paddingHorizontal:30,
-  // ARREDONDA AS BORDAS
- borderRadius:12,
-  // ESPAÇO ENTRE OS BOTÕES
- marginHorizontal:10,
- },
+    marginBottom: 20,
+  },
+  // ESTILOS DOS BOTÕES +1 E -1
+  botao: {
+    // COR DO BOTÃO 
+    backgroundColor: '#00ff88',
+    // ESPAÇAMENTO VERTICAL
+    paddingVertical: 15,
+    // ESPAÇAMENTO HORIZONTAL
+    paddingHorizontal: 30,
+    // ARREDONDA AS BORDAS
+    borderRadius: 12,
+    // ESPAÇO ENTRE OS BOTÕES
+    marginHorizontal: 10,
+  },
 
- //ESTILO DO BOTÃO RESETAR
- botaoReset:{
-  // COR VERMELHA
-  backgroundColor:'#d40b00ff',
-  // ESPAÇAMENTOS
-  paddingVertical:15,
-  paddingHorizontal:40,
-  // ARREDONDA AS BORDAS
-  borderRadius:12,
- },
+  //ESTILO DO BOTÃO RESETAR
+  botaoReset: {
+    // COR VERMELHA
+    backgroundColor: '#ff0000f8',
+    // ESPAÇAMENTOS
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    // ARREDONDA AS BORDAS
+    borderRadius: 12,
+  },
 
- // ESTILO DOS TEXTOS DOS BOTÕES 
- textoBotao:{
-  // TAMANHO DA LETRA
-  fontSize:22,
-  // COR DO TEXTO
-  color:'#181818ff',
-  // NEGRITO
-  fontWeight:'bold',
+  // ESTILO DOS TEXTOS DOS BOTÕES 
+  textoBotao: {
+    // TAMANHO DA LETRA
+    fontSize: 22,
+    // COR DO TEXTO
+    color: '#000',
+    // NEGRITO
+    fontWeight: 'bold',
 
- },
+  },
+  // área de jogadores 
+  areaJogadores: {
+    flexDirection: 'row',
+    // espaço entre eles
+    grap: 15,
+
+  },
+
+  areaJogador: {
+    backgroundColor: '#1f1f1f',
+    width: 230,
+    padding: 15,
+    borderRadius: 15,
+    alignItems: 'center',
+  },
+
+  input: {
+    backgroundColor: '#fff',
+    width: '100%',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+    fontSize: 16,
+
+  },
+
+  nome: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+
+  },
+
+  lider: {
+    fontSize: 22,
+    color: '#fff',
+    marginBottom: 20,
+    fontWeight: 'bold',
+  }
 
 
- });
+});
